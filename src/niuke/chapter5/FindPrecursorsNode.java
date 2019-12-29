4186a833c6c3e32aa479e3b634c4f996bd7e8b9f
yan.zhang <13721133018@163.com>
2019-12-29 11:25:41 +0800

    前驱节点

--- src/niuke/chapter5/FindPrecursorsNode.java
+++ src/niuke/chapter5/FindPrecursorsNode.java
@@ -19,6 +19,61 @@ public class FindPrecursorsNode {
      * 2.如果没有左子树，前驱节点: 向上搜索，当前节点是父节点的右节点停止
      */
     public static void main(String[] args) {
+        Node head = new Node(1);
+        head.left = new Node(2);
+        head.left.parent = head;
+        head.right = new Node(3);
+        head.right.parent = head;
 
+        head.left.left = new Node(4);
+        head.left.left.parent = head.left;
+        head.left.right = new Node(5);
+        head.left.right.parent = head.left;
+        head.right.left = new Node(6);
+        head.right.left.parent = head.right;
+        head.right.right = new Node(7);
+        head.right.right.parent = head.right;
+
+        System.out.println(getPrecursorsNode(head.right.left).getValue());
+    }
+
+
+    private static Node getPrecursorsNode(Node node) {
+        if (null == node) {
+            return node;
+        }
+        if (node.left != null) {
+            return getLeftTreeMost(node.left);
+        } else {
+            Node parNode = node.parent;
+            while (parNode != null && parNode.right != node) {
+                node = parNode;
+                parNode = parNode.parent;
+            }
+            return parNode;
+        }
+    }
+
+    private static Node getLeftTreeMost(Node leftNode) {
+        if (null == leftNode.right) {
+            return leftNode;
+        }
+        return getLeftTreeMost(leftNode.right);
+    }
+
+
+    private static class Node {
+        private int value;
+        public FindPrecursorsNode.Node left;
+        public FindPrecursorsNode.Node right;
+        public FindPrecursorsNode.Node parent;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
     }
 }
