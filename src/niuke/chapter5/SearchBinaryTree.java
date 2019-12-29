7ee7085189d810a075b91dee7a4393885885cb45
yan.zhang <13721133018@163.com>
2019-12-29 23:22:17 +0800

    1.是否为完全二叉树2.完全二叉树节点个数

---
+++ src/niuke/chapter5/SearchBinaryTree.java
@@ -0,0 +1,21 @@
+/*
+ * All rights Reserved, Designed By baowei
+ *
+ * 注意：本内容仅限于内部传阅，禁止外泄以及用于其他的商业目的
+ */
+package src.niuke.chapter5;
+
+/**
+ * @author yan.zhang
+ * @date 2019/12/29 16:49
+ */
+
+/**
+ * 搜索二叉树：对于这颗树上的任何一个节点为头的子树，左子树都比该头小，右子树都比该头大
+ * 通常情况下，搜索二叉树不含有重复节点，可以将重复的节点压缩成一个List
+ * 判断一颗树是否为搜索二叉树
+ * 1.思路：中序遍历，节点依次升序，就是搜索二叉树
+ */
+public class SearchBinaryTree {
+
+}
