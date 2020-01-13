a1ed422b9f43eb337c5ec62a9bb0db711baf1bb7
yan.zhang <13721133018@163.com>
2020-01-13 17:13:10 +0800

    并查集

--- src/niuke/chapter6/BlondFilter.java
+++ src/niuke/chapter6/BlondFilter.java
@@ -13,9 +13,27 @@ package src.niuke.chapter6;
 /**
  * 布隆布隆过滤器
  * 本质是查询某数据是否在一个集合中
+ * 存在失误率
  */
 public class BlondFilter {
     public static void main(String[] args) {
+        blondFilter();
+    }
 
+    private static void blondFilter() {
+        /*long[] arr = new long[1000]; //1000*64=64000   64000bit大小
+
+        long target = 30000;
+
+        int longArrIndex = (int) (target / 64); //arr数组桶位置
+
+
+        int bitIndex = (int) (target % 64); //对应桶内的bit位置
+
+        System.out.println(arr[longArrIndex]);
+
+        arr[longArrIndex] = (arr[longArrIndex] | (1 << bitIndex));
+
+        System.out.println(arr[longArrIndex]);*/
     }
 }
--- src/niuke/chapter6/RandomPool.java
+++ src/niuke/chapter6/RandomPool.java
@@ -16,8 +16,8 @@ import java.util.Map;
 /**
  * 哈希函数特性：
  * 1.输入域无穷大
- * 2.输出域是一个固定区间
- * 3.输入一样，输出一定一样
+ * 2.输出域是一个有穷（固定）区间，可以认为输入域远大于输出域
+ * 3.输入一样，输出一定一样 （重要）
  * 4.当输入(无穷大域)不一样，也有可能得到输出(固定域)一致，成为哈希碰撞
  * 5.哈希函数离散性，（在输出域上均匀分布）
  * 6.推论:输出域上均匀分布，输出域对m取模，则在0~m-1上也均匀分布，ag：0~98上均匀分布，对3取模，则在0，1，2上均匀分布
@@ -36,8 +36,17 @@ public class RandomPool<K> {
     private int size;
 
     public static void main(String[] args) {
+        RandomPool<String> randomPool = new RandomPool<>();
+
+        randomPool.insert("str1");
+        randomPool.insert("str2");
+
+        randomPool.delete("str1");
+        System.out.println(randomPool.keyIndexMap);
+        System.out.println(randomPool.IndexKeyMap);
 
     }
+
 
     private K getRandom() {
         if (size == 0) return null;
@@ -53,7 +62,6 @@ public class RandomPool<K> {
             Integer deleteIndex = keyIndexMap.get(key);
             int lastIndex = --size;
             K lastKey = IndexKeyMap.get(lastIndex);
-            keyIndexMap.put(lastKey, lastIndex);//
             IndexKeyMap.put(deleteIndex, lastKey);//IndexKeyMap在被删除的bucket上存在最后一个bucket的值
             keyIndexMap.remove(key);
             IndexKeyMap.remove(lastIndex);//IndexKeyMap删除掉最后一个，保证在[0,size]区间连续
