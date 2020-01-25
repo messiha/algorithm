56fb5d6cf256d39c239e8487965fdef13eb9775e
yan.zhang <13721133018@163.com>
2020-01-25 19:14:12 +0800

    岛问题

--- src/niuke/chapter6/IsLand.java
+++ src/niuke/chapter6/IsLand.java
@@ -23,6 +23,37 @@ package src.niuke.chapter6;
 public class IsLand {
 
     public static void main(String[] args) {
+        int[][] isLand = {{0, 0, 1, 0, 1, 0}, {1, 1, 1, 0, 1, 0}, {1, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 0}};
+        int count = countIsLand(isLand);
+        System.out.println(count);
+    }
 
+    private static int countIsLand(int[][] isLand) {
+        int M = isLand.length;
+        int N = isLand[0].length;
+        int count = 0;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (isLand[i][j] == 1) {
+                    count++;  //岛屿个数+1，将周边数字感染
+                    infect(isLand, i, j, M, N);
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
+     * 递归完成感染函数
+     */
+    private static void infect(int[][] isLand, int i, int j, int m, int n) {
+        if (i < 0 || j < 0 || i >= m || j >= n || isLand[i][j] != 1) {
+            return;
+        }
+        isLand[i][j] = 2;
+        infect(isLand, i + 1, j, m, n);
+        infect(isLand, i - 1, j, m, n);
+        infect(isLand, i, j + 1, m, n);
+        infect(isLand, i, j - 1, m, n);
     }
 }
