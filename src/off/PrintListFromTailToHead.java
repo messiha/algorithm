865c24cfc7a3ce4e93e76d2db395ebe4d8aba713
yan.zhang <13721133018@163.com>
2020-03-19 11:19:48 +0800

    从头置尾打印链表

--- src/off/PrintListFromTailToHead.java
+++ src/off/PrintListFromTailToHead.java
@@ -1,11 +1,26 @@
-package src.exercise;
+/*
+ * All rights Reserved, Designed By baowei
+ *
+ * 注意：本内容仅限于内部传阅，禁止外泄以及用于其他的商业目的
+ */
+package src.off;
 
 import src.datastruct.SingleListNode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Stack;
 
-public class Practice4 {
+/**
+ * @author yan.zhang
+ * @date 2019/8/10 22:20
+ */
+
+/**
+ * 输入一个链表，按链表值从尾到头的顺序返回一个ArrayList。
+ */
+public class PrintListFromTailToHead {
 
     private static ArrayList<Integer> list = new ArrayList<Integer>();
 
@@ -13,11 +28,11 @@ public class Practice4 {
         SingleListNode n1 = new SingleListNode(1);
         SingleListNode n2 = new SingleListNode(2);
         n1.setNext(n2);
-        ArrayList<Integer> integers = printByInsertHead(n1);
+        ArrayList<Integer> integers = printListFromTailToHead(n1);
         System.out.println(Arrays.toString(integers.toArray()));
     }
 
-    public static ArrayList<Integer> printByInsertHead(SingleListNode node) {
+    public static ArrayList<Integer> printByListReverse(SingleListNode node) {
         ArrayList<Integer> list = new ArrayList<Integer>();
         SingleListNode head = node;
         while (head != null) {
@@ -46,6 +61,23 @@ public class Practice4 {
         list.set(j, tmp);
     }
 
+    public static ArrayList<Integer> printListFromTailToHead(SingleListNode singleListNode) {
+        //思路2 栈
+        Stack<Integer> stack = new Stack<>();
+        while (singleListNode != null) {
+            stack.push(singleListNode.val);
+            singleListNode = singleListNode.next;
+
+        }
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        while (!stack.empty()) {
+            list.add(stack.pop());
+        }
+        return list;
+
+
+    }
+
     public static ArrayList<Integer> printByRecur(SingleListNode singleListNode) {
         if (singleListNode != null) {
             ArrayList<Integer> list = printByRecur(singleListNode.next);
@@ -53,4 +85,21 @@ public class Practice4 {
         }
         return list;
     }
+
+
+    public static ArrayList<Integer> printByInsertHead(SingleListNode node) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        SingleListNode head = node;
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        Collections.reverse(list);
+        return list;
+    }
+
+
 }
+
+
+
