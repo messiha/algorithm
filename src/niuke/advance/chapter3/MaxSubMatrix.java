61c61689ff5d90766919c0bb39d7adc2b642db96
yan.zhang <13721133018@163.com>
2020-07-05 22:31:56 +0800

    最大子矩阵问题求解

--- src/niuke/advance/chapter3/MaxSubMatrix.java
+++ src/niuke/advance/chapter3/MaxSubMatrix.java
@@ -34,10 +34,36 @@ public class MaxSubMatrix {
      */
     public static void main(String[] args) {
 
-        int[] arr = {4, 3, 2, 5, 6};
-        System.out.println(maxRecFromBottom(arr));
+//        int[] arr = {4, 3, 2, 5, 6};
+//        System.out.println(maxRecFromBottom(arr));
+
+        int[][] map = new int[][]{{1, 0, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 0}};
+        System.out.println(maxRecSize(map));
 
     }
+
+    /**
+     * 对于二位数组的每一列都需要计算最大面积（可能存在很长的一列，存在这一列就是最大面积的可能性）
+     */
+    private static int maxRecSize(int[][] map) {
+        if (null == map || map.length == 0 || map[0].length == 0) {
+            return 0;
+        }
+        int maxArea = 0;
+
+        //辅助数组，对应二维数组的列，用于合并二维数组
+        int[] help = new int[map[0].length];
+
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                //矩阵"合并",合并成一个数组
+                help[j] = map[i][j] == 0 ? 0 : help[j] + 1;
+            }
+            maxArea = Math.max(maxRecFromBottom(help), maxArea);
+        }
+        return maxArea;
+    }
+
 
     /**
      * 用数组标识直方图，求直方图中最大正方形面积，直方图问题求解函数
@@ -54,12 +80,11 @@ public class MaxSubMatrix {
 
         for (int i = 0; i < arr.length; i++) {
             while (!stack.isEmpty() && arr[i] <= arr[stack.peek()]) {
-                Integer j = stack.pop();
+                int j = stack.pop();
                 int k = stack.isEmpty() ? -1 : stack.peek();
                 //不满足单调栈 栈底到栈顶由小到大的顺序，当不满足该条件，意味着：当前栈顶的元素移动范围确定。
                 //eg:[5,6,3,7,2] 当3入栈，条件不满足，（此时单调栈可快速得到6左侧最近比6小的数5，6右侧最近比6小的数3），栈中元素弹出计算最大面积
                 int curArea = (i - k - 1) * arr[j];
-
                 maxArea = Math.max(curArea, maxArea);
             }
             stack.push(i);
@@ -76,3 +101,5 @@ public class MaxSubMatrix {
         return maxArea;
     }
 }
+
+
