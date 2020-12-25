3de6266f038f686f91dbbceaa13af4cf043abb2d
yan.zhang <13721133018@163.com>
2020-12-25 14:30:02 +0800

    堆排序思路求第K大数

--- src/off/heap/FindKth.java
+++ src/off/heap/FindKth.java
@@ -16,65 +16,66 @@ public class FindKth {
      * @param args
      */
     public static void main(String[] args) {
-        int[] arr1 = new int[]{1, 3, 5, 2, 2};
-       /* int[] arr2 = new int[]{1332802, 1177178, 1514891, 871248, 753214, 123866, 1615405, 328656, 1540395, 968891, 1884022, 252932, 1034406, 1455178, 821713, 486232, 860175, 1896237, 852300, 566715, 1285209, 1845742, 883142, 259266, 520911, 1844960, 218188, 1528217, 332380, 261485, 1111670, 16920, 1249664, 1199799, 1959818, 1546744, 1904944, 51047, 1176397, 190970, 48715, 349690, 673887, 1648782, 1010556, 1165786, 937247, 986578, 798663};
-        System.out.println(findKth(arr2, 49, 24));
-        System.out.println(Arrays.toString(arr2));*/
-
-        findKthByHeap(arr1, 0, 0);
-        System.out.println(Arrays.toString(arr1));
+        int[] arr2 = new int[]{1332802, 1177178, 1514891, 871248, 753214, 123866, 1615405, 328656, 1540395, 968891, 1884022, 252932, 1034406, 1455178, 821713, 486232, 860175, 1896237, 852300, 566715, 1285209, 1845742, 883142, 259266, 520911, 1844960, 218188, 1528217, 332380, 261485, 1111670, 16920, 1249664, 1199799, 1959818, 1546744, 1904944, 51047, 1176397, 190970, 48715, 349690, 673887, 1648782, 1010556, 1165786, 937247, 986578, 798663};
+        System.out.println(findKthByHeap(arr2, 49, 24));
+        System.out.println(Arrays.toString(arr2));
     }
 
 
     /**
      * 类比快排，但是在每一次partition之后使用二分查找，减少一般的排序
-     *
-     * @param arr
-     * @param n
-     * @param K
-     * @return
      */
     private static int findKth(int[] arr, int n, int K) {
         return 0;
     }
 
+
+    /**
+     * 方式二：
+     * 根据堆排序的思路。做升序排序，可以做到只排序到K位置以及K之后的位置，可以找出解
+     */
     private static int findKthByHeap(int[] arr, int n, int K) {
         for (int i = 0; i < arr.length; i++) {
             heapInsert(arr, i);
         }
+        int heapSize = n;
+        //最后一个位置和堆顶交换,并将堆大小 -1
+        swap(arr, 0, --heapSize);
 
-        int size = arr.length;
-
-        swap(arr, 0, --size);
-
-        while (size > 0) {
-            //找第K大则只需要调整K次
-            heapify(arr, 0, size);
-            swap(arr, 0, --size);
-
+        //
+        while (heapSize > 0 && K > 0) {
+            heapify(arr, 0, heapSize);
+            swap(arr, 0, --heapSize);
+            K--;
         }
-        return 0;
 
+        return arr[heapSize + 1];
     }
 
-    private static void heapify(int[] arr, int size, int i) {
-        int left = i * 2 + 1;
-        while (left < size) {
-            int small = left + 1 < size && arr[left] < arr[left + 1] ? arr[left] : arr[left + 1];
-            small = arr[i] < arr[small] ? i : small;
+    private static void heapify(int[] arr, int index, int heapSize) {
+        int left = 2 * index + 1;
+        while (left < heapSize) {
+            int largest = left + 1 < heapSize && arr[left + 1] > arr[left]
+                    ? left + 1
+                    : left;
+            largest = arr[largest] > arr[index] ? largest : index;
 
-            if (small == i) {
+            if (largest == index) {
                 break;
             }
-
-            swap(arr, small, i);
-            i = small;
-            left = i * 2 + 1;
+            swap(arr, index, largest);
+            index = largest;
+            left = 2 * index + 1;
         }
 
     }
 
-
+    /**
+     * 由小到大升序排列，建立大根堆
+     *
+     * @param arr
+     * @param index
+     */
     private static void heapInsert(int[] arr, int index) {
         while (arr[index] > arr[(index - 1) / 2]) {
             swap(arr, index, (index - 1) / 2);
