dcf7381dc4a631b0bff46cae28030ca58784bc52
yan.zhang <13721133018@163.com>
2022-02-25 21:36:21 +0800

    全排列问题

--- src/leetcode/trace/Permutations.java
+++ src/leetcode/trace/Permutations.java
@@ -13,11 +13,11 @@ public class Permutations {
     /**
      * https://leetcode-cn.com/problems/permutations/
      * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列。
-     *
+     * <p>
      * https://leetcode-cn.com/problems/permutations/solution/hui-su-suan-fa-python-dai-ma-java-dai-ma-by-liweiw/
      */
     public static void main(String[] args) {
-        int[] ints = {1, 2, 3};
+        int[] ints = {1, 1, 2};
         solution_01(ints);
     }
 
@@ -28,10 +28,7 @@ public class Permutations {
      * 层级、深度：level
      * 已选择的数：path
      * 记录是否已选择：used[]
-     *
-     * @return
      */
-
     private static List<List<Integer>> solution_01(int[] nums) {
         int len = nums.length;
         List<List<Integer>> res = new ArrayList<>();
@@ -42,12 +39,43 @@ public class Permutations {
         Stack<Integer> path = new Stack<>();
         boolean[] used = new boolean[len];
 
-        dfs(nums, len, 0, path, used, res);
+//        dfs_0(nums, len, 0, path, used, res);
+        dfs_1(nums, len, 0, path, used, res);
 
         return res;
     }
 
-    private static void dfs(int[] ints, int len, int depth, Stack<Integer> path, boolean[] used, List<List<Integer>> res) {
+
+    //深度优先+不回溯
+
+    private static void dfs_1(int[] nums, int len, int depth, List<Integer> path, boolean[] used, List<List<Integer>> res) {
+        if (depth == len) {
+            //每一层传递下来的 path 变量都是新建的
+            res.add(path);
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            //没有被使用
+            if (!used[i]) {
+                //newPath保存上次
+                List<Integer> newPath = new ArrayList<>(path);
+                newPath.add(nums[i]);
+                //每一次尝试都创建新的变量表示当前的"状态"
+                boolean[] newUsed = new boolean[len];
+                System.arraycopy(used, 0, newUsed, 0, len);
+
+                newUsed[i] = true;
+
+                dfs_1(nums, len, depth + 1, newPath, newUsed, res);
+            }
+        }
+
+
+    }
+
+    //深度优先+回溯
+    private static void dfs_0(int[] ints, int len, int depth, Stack<Integer> path, boolean[] used, List<List<Integer>> res) {
         if (depth == len) {
             res.add(new ArrayList<>(path));
             return;
@@ -60,7 +88,7 @@ public class Permutations {
             path.push(ints[i]);
             used[i] = true;
 
-            dfs(ints, len, depth + 1, path, used, res);
+            dfs_0(ints, len, depth + 1, path, used, res);
 
             //回溯
             path.pop();
---
+++ src/leetcode/trace/permutationsII.java
@@ -0,0 +1,60 @@
+package src.leetcode.trace;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @Author yan.zhang
+ * @Date 2022/2/25 20:08
+ * @Version 1.0
+ */
+public class permutationsII {
+    /**
+     * https://leetcode-cn.com/problems/permutations-ii/
+     * 给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+     * https://leetcode-cn.com/problems/permutations-ii/solution/hui-su-suan-fa-python-dai-ma-java-dai-ma-by-liwe-2/
+     */
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 1, 2};
+        solution(nums);
+    }
+
+    private static List<List<Integer>> solution(int[] nums) {
+        //排序，目的是剪枝
+        Arrays.sort(nums);
+
+        int len = nums.length;
+        boolean[] used = new boolean[len];
+        List<List<Integer>> res = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        dfs(nums, len, 0, used, stack, res);
+        return res;
+    }
+
+    private static void dfs(int[] nums, int len, int depth, boolean[] used, Stack<Integer> stack, List<List<Integer>> res) {
+        if (depth == len) {
+            res.add(new ArrayList<>(stack));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+
+            //剪枝
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+                continue;
+            }
+
+            stack.push(nums[i]);
+            used[i] = true;
+
+            dfs(nums, len, depth + 1, used, stack, res);
+
+            stack.pop();
+            used[i] = false;
+        }
+    }
+}
