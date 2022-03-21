6e627800f8c367d19dc97726ffef939e911dbed6
yan.zhang <13721133018@163.com>
2022-03-21 22:49:23 +0800

    1.括号生成dp方式

--- src/leetcode/trace/GenerateParenthesis.java
+++ src/leetcode/trace/GenerateParenthesis.java
@@ -1,7 +1,6 @@
 package src.leetcode.trace;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author yan.zhang
@@ -13,7 +12,8 @@ public class GenerateParenthesis {
      * 括号生成
      */
     public static void main(String[] args) {
-        solution(2);
+//        solution(3);
+        dp(3);
     }
 
     private static List<String> solution(int n) {
@@ -54,4 +54,77 @@ public class GenerateParenthesis {
         }
 
     }
+
+    /**
+     * 动态规划思路
+     * 思路：https://www.bilibili.com/video/BV1wq4y1u7Q2?spm_id_from=333.337.search-card.all.click
+     */
+    private static List<String> dp(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        //定义dp[n],dp[n]代表n对括号时,所有有效括号的组合
+        List<String>[] dp = new List[n + 1];
+        dp[1] = new ArrayList<>();
+        dp[1].add("()");
+
+        Set<String> set = new HashSet<>();
+
+        //计算n >= 2 情况
+        for (int i = 2; i <= n; i++) {
+            //1.初始化
+            dp[i] = new ArrayList<>();
+            //2.遍历上一级结果集 举例当n = 3 dp[2]取出的结果集为()(),(())两个
+            for (int j = 0; j < dp[i - 1].size(); j++) {
+                String base = dp[i - 1].get(j);
+                //3.对base遍历,将()塞入base每个字符的间隙,举例 base = ()(),依次添加(),得到结果为()()(),(())()等等等
+                for (int k = 0; k < base.length(); k++) {
+                    String left = base.substring(0, k);
+                    String right = base.substring(k);
+                    String add = left + "()" + right;
+                    //去重
+                    if (set.add(add)) {
+                        dp[i].add(add);
+                    }
+
+                }
+            }
+        }
+        return dp[n];
+    }
+
+    /**
+     * dp思路不去重版本
+     *
+     * @param n
+     * @return
+     */
+    private static List<String> dp_1(int n) {
+        LinkedList<LinkedList<String>> result = new LinkedList<>();
+        if (n == 0)
+            return result.get(0);
+        LinkedList<String> list0 = new LinkedList<>();
+        list0.add("");
+        result.add(list0);
+        LinkedList<String> list1 = new LinkedList<>();
+        list1.add("()");
+        result.add(list1);
+        for (int i = 2; i <= n; i++) {
+            LinkedList<String> temp = new LinkedList<>();
+            for (int j = 0; j < i; j++) {
+                List<String> str1 = result.get(j);
+                List<String> str2 = result.get(i - 1 - j);
+                for (String s1 : str1) {
+                    for (String s2 : str2) {
+                        String el = "(" + s1 + ")" + s2;
+                        temp.add(el);
+                    }
+                }
+
+            }
+            result.add(temp);
+        }
+        return result.get(n);
+    }
+
 }
